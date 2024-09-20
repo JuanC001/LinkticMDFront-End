771b7ed69f828e34db8bae46b8c2aa3ae18c11c6
Juan <jcflorezpaez@gmail.com>
2024-09-19 23:04:12 -0500

    Se instaló un sistema de alertas sencillo para alertas simples + Se termino el sistema de registro

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,45 +1,58 @@
 import { Box, Button, TextField, Typography } from "@mui/material"
 import { useState } from "react"
+import { RegisterModal } from "../../Global/Components/RegisterModal/RegisterModal"
 
 export const Login = () => {
 
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
 
-    const handleLogin = () => {}
+    const [open, setOpen] = useState(false)
+
+    const handleRegister = () => { }
+
+    const handleLogin = () => { }
 
     return (
-        <Box height={'85vh'} display={'flex'} flexDirection={'column'}
-            alignItems={'center'} justifyContent={'center'}>
+        <>
+            <Box height={'85vh'} display={'flex'} flexDirection={'column'}
+                alignItems={'center'} justifyContent={'center'}>
 
-            <Typography variant="h4" gutterBottom>
-                Login
-            </Typography>
-            <TextField
-                label="Email"
-                variant="outlined"
-                margin="normal"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                sx={{ width: '70%' }}
-            />
-            <TextField
-                label="Password"
-                type="password"
-                variant="outlined"
-                margin="normal"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                sx={{ width: '70%' }}
-            />
-            <Button
-                variant="contained"
-                color="primary"
-                onClick={handleLogin}
-                sx={{ mt: 2, width: '70%' }}
-            >
-                Login
-            </Button>
-        </Box>
+                <Typography variant="h4" gutterBottom>
+                    Login
+                </Typography>
+                <TextField
+                    label="Email"
+                    variant="outlined"
+                    margin="normal"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    sx={{ width: '70%' }}
+                />
+                <TextField
+                    label="Password"
+                    type="password"
+                    variant="outlined"
+                    margin="normal"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    sx={{ width: '70%' }}
+                />
+                <Typography variant="body2" gutterBottom>
+                    ¿No tienes cuenta? <span style={{textDecoration: 'underline', cursor: 'pointer'}} onClick={() => setOpen(true)}>
+                        Registrate
+                    </span>
+                </Typography>
+                <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={handleLogin}
+                    sx={{ mt: 2, width: '70%' }}
+                >
+                    Login
+                </Button>
+            </Box>
+            <RegisterModal open={open} handleClose={setOpen} />
+        </>
     )
 }
