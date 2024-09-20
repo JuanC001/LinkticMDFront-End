65cc66bbd23b1e0e6b6177bc00dafeb5fce46245
Juan <jcflorezpaez@gmail.com>
2024-09-20 10:05:10 -0500

    Configuración de Headers como el token para autenticación

--- src/api/linkticApi.js
+++ src/api/linkticApi.js
@@ -6,4 +6,13 @@ const linkticApi = axios.create({
     baseURL: API_URL,
 })
 
+linkticApi.interceptors.request.use((config) => {
+    const { token } = sessionStorage.getItem('user') ? JSON.parse(sessionStorage.getItem('user')) : {}
+
+    if (token) {
+        config.headers['x-token'] = token
+    }
+    return config
+})
+
 export default linkticApi;
